5e0a856134403d521284f0f49d182834d0f4542e
Free Server contributor 1 <free.server.github@gmail.com>
2018-12-14 10:27:16 +0800

    e: Enhance renew free-server with only perform git fetch and check hash on fly

--- bin/renew-update-free-server.sh
+++ bin/renew-update-free-server.sh
@@ -10,11 +10,14 @@ main() {
 
 mainUpdate() {
   cd ${gitRepoFreeServerPath}
-  git stash
+
+  git fetch
   currentGitCommitHash=$(git rev-parse HEAD)
-  git pull
-  newGitCommitHash=$(git rev-parse HEAD)
+  newGitCommitHash=$(git rev-parse master@{upstream})
+
   if [[ "${currentGitCommitHash}" != "${newGitCommitHash}" ]];then
+    git stash
+    git pull
     mailNotify "Updating - Safe server update started at $(getCurrentDateTime)"
     /bin/bash ./install-shadowsocks-spdy.sh
     mailNotify "Updated - Safe server update finished at $(getCurrentDateTime)"
