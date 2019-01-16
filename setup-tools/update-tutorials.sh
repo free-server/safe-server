1522c9456a3bff4bece170aa7d7627377a4b21d7
Free Server contributor 1 <free.server.github@gmail.com>
2019-01-16 21:43:58 +0800

    f: Update SwitchOmega default proxy domain

--- setup-tools/update-tutorials.sh
+++ setup-tools/update-tutorials.sh
@@ -10,10 +10,13 @@ main() {
 updateServerUrl() {
   # serverUrlPlaceholder=https://server1.free-server.me:443/
   export serverUrlPlaceholder=__SERVER_URL__
+  export serverNamePlaceholder=__SERVER_DOMAIN_NAME__
 
   echoS "Update Tutorial: replace __SERVER_URL__ to ${httpsFreeServerUrl}"
+  echoS "Update Tutorial: replace __SERVER_DOMAIN__ to ${freeServerName}"
   # miscDir
   find ${miscDir} -type f -exec bash -c "replaceStringInFile {} ${serverUrlPlaceholder} ${httpsFreeServerUrl} " \;
+  find ${miscDir} -type f -exec bash -c "replaceStringInFile {} ${serverNamePlaceholder} ${freeServerName} " \;
 
 }
 
