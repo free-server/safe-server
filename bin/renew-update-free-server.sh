d9de26af79b2dd8e36f13fe6f97623923999f914
Free Server contributor 1 <free.server.github@gmail.com>
2019-05-01 15:25:47 +0800

    b: Should restart HTTP/2 SSR after upgrade

--- bin/renew-update-free-server.sh
+++ bin/renew-update-free-server.sh
@@ -20,8 +20,9 @@ mainUpdate() {
     git pull
     mailNotify "[Start] Safe server update started at $(getCurrentDateTime)"
     /bin/bash ./install-shadowsocks-spdy.sh
-    /bin/bash ./bin/restart-shadowsocks-r.sh
-    /bin/bash ./bin/restart-spdy-nghttpx-squid.sh
+    /bin/bash ${setupToolsDir}/update-tutorials.sh
+    /bin/bash ${binDir}/restart-shadowsocks-r.sh
+    /bin/bash ${binDir}/restart-spdy-nghttpx-squid.sh
     mailNotify "[End] Safe server update finished at $(getCurrentDateTime)"
   else
     echoErr "[WARN] FreeServerUpdate: The git remote ${freeServerRepo} is identical as local git repo ${gitRepoFreeServerPath}"
