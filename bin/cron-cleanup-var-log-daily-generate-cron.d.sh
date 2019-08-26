5b6ab491bac7d8e1ac6cba57d8c0e538c44c2a63
Free Server contributor 1 <free.server.github@gmail.com>
2019-08-26 14:36:45 +0800

    e: Cleanup azure waagent which will consume all inodes

--- bin/cron-cleanup-var-log-daily-generate-cron.d.sh
+++ bin/cron-cleanup-var-log-daily-generate-cron.d.sh
@@ -5,13 +5,21 @@ source /opt/.global-utils.sh
 fileSize="+100M"
 ## file to write to cron.d
 file="/etc/cron.d/free-server-cron-cleanup-var-log-daily"
+
+
+file2AzureLog="/etc/cron.d/free-server-cron-cleanup-var-log-daily-2-azure"
 fileDaily="/etc/cron.daily/free-server-cron-cleanup-var-log-daily"
 
-## restart daily
+## clear big log files
 shellCommand="find /var/log -size ${fileSize} | sort -rh | xargs rm"
+
+## clear Azure Logs which will consume all inode
+shellCommand2AzureLog="rm -rf /var/lib/waagent/history/"
 
 ## restart process every day at 2pm GMT0
 echo "18 17 * * * root ${shellCommand}" > ${file}
+
+echo "19 17 * * * root ${shellCommand2AzureLog}" > ${file2AzureLog}
 
 echo '#!/bin/sh' > ${fileDaily}
 echo "${shellCommand}" >> ${fileDaily}
@@ -19,6 +27,9 @@ chmod a+x ${fileDaily}
 
 echo "Done, cat ${file}"
 cat ${file}
+
+echo "Done, cat ${file2AzureLog}"
+cat ${file2AzureLog}
 
 echo "Done, cat ${fileDaily}"
 cat ${fileDaily}
